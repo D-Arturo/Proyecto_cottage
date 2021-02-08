7028b223bb0d23664414c9cdc29b284bc207899a
PablinVaz <57219639+PablinVaz@users.noreply.github.com>
2021-02-08 17:48:22 +0100

    footer

--- scripts/faqs.js
+++ scripts/faqs.js
@@ -4,11 +4,9 @@ this.addEventListener("DOMContentLoaded", () =>{
   
         if(question.parentNode.classList.contains("active")){
             question.parentNode.classList.toggle("active")
+        }else{
+            questions.forEach(question => question.parentNode.classList.remove("active"))
+            question.parentNode.classList.add("active")
         }
-    else{
-    questions.forEach(question => question.parentNode.classList.remove("active"))
-    question.parentNode.classList.add("active")
-        }
-
     }))
 }) 
